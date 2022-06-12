03638ed7f78b4a4ebda2d15933ae80a0e1e7627c
AttiaYoussef <youssef.attia@epfl.ch>
2022-06-12 10:35:48 +0200

    Push

---
+++ Helpers.py
@@ -0,0 +1,75 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#### Grad norm plot
+def plot_grad_norms_per_optimizer(optimizer_name, grad_norms, epochs):
+  x = list(range(epochs))
+  y = grad_norms
+  plt.xlabel("Epochs")
+  plt.ylabel("Gradient norm")
+  plt.title("Gradient norms for {0} epochs for the ".format(epochs) + optimizer_name + " optimizer")
+  plt.plot(x,y)
+  plt.show()
+
+
+def norm_gradient(model):
+    total_norm = 0
+    for p in model.parameters():
+        param_norm = p.grad.data.norm(2)
+        total_norm += param_norm.item() ** 2
+    return total_norm ** (0.5)
+
+
+##### Loss landscape plot
+
+
+##### Density plot
+
+
+def get_esd_plot(eigenvalues, weights):
+    density, grids = density_generate(eigenvalues, weights)
+    plt.semilogy(grids, density + 1.0e-7)
+    plt.ylabel('Density (Log Scale)', fontsize=14, labelpad=10)
+    plt.xlabel('Eigenvlaue', fontsize=14, labelpad=10)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.axis([np.min(eigenvalues) - 1, np.max(eigenvalues) + 1, None, None])
+    plt.tight_layout()
+    plt.show()
+    #plt.savefig('example.pdf')
+
+
+def density_generate(eigenvalues,
+                     weights,
+                     num_bins=10000,
+                     sigma_squared=1e-5,
+                     overhead=0.01):
+
+    eigenvalues = np.array(eigenvalues)
+    weights = np.array(weights)
+
+    lambda_max = np.mean(np.max(eigenvalues, axis=1), axis=0) + overhead
+    lambda_min = np.mean(np.min(eigenvalues, axis=1), axis=0) - overhead
+
+    grids = np.linspace(lambda_min, lambda_max, num=num_bins)
+    sigma = sigma_squared * max(1, (lambda_max - lambda_min))
+
+    num_runs = eigenvalues.shape[0]
+    density_output = np.zeros((num_runs, num_bins))
+
+    for i in range(num_runs):
+        for j in range(num_bins):
+            x = grids[j]
+            tmp_result = gaussian(eigenvalues[i, :], x, sigma)
+            density_output[i, j] = np.sum(tmp_result * weights[i, :])
+    density = np.mean(density_output, axis=0)
+    normalization = np.sum(density) * (grids[1] - grids[0])
+    density = density / normalization
+    return density, grids
+
+
+def gaussian(x, x0, sigma_squared):
+    return np.exp(-(x0 - x)**2 /
+                  (2.0 * sigma_squared)) / np.sqrt(2 * np.pi * sigma_squared)
