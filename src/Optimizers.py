38162eda3ba608cd431620621d9b2a2d1747240c
AttiaYoussef <youssef.attia@epfl.ch>
2022-06-15 21:06:01 +0200

    Change some files

--- src/Optimizers.py
+++ src/Optimizers.py
@@ -18,11 +18,11 @@ def get_optimizer(model, name = 'sgd', lr = 0.1, momentum = 0.0):
     ### First order optimizers
     
     if name == 'adam':
-        return torch.optim.Adam(model.parameters(), lr = lr)
+        return torch.optim.Adam(model.parameters(), lr = 0.004722222222222222 ,betas=(0.675 ,0.999))
     if name == 'sgd':
         return torch.optim.SGD(model.parameters(), lr = lr, momentum = momentum)
     if name == 'adagrad':
-        return torch.optim.Adagrad(model.parameters(), lr = lr)
+        return torch.optim.Adagrad(model.parameters(), lr = 0.0012)
     
     ### Second order optimizers
     if name == 'adahessian':
